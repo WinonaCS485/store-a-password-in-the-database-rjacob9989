610a13b1494dc1d2dc7b54539519220d22d6e486
rjacob9989 <rhettjjacobs@gmail.com>
2020-02-11 16:03:39 -0600

    Password Validation Working
    Password Validation Working

--- password_test.py
+++ password_test.py
@@ -1,7 +1,5 @@
 import pymysql.cursors
-import hashlib
-import base64
-import uuid
+import hashlib, uuid
 
 # Connect to the database
 connection = pymysql.connect(host='mrbartucz.com',
@@ -19,19 +17,19 @@ try:
         print("Username: " + user_name)
 
         password = input("Enter a password:\n" )
-        salt = base64.urlsafe_b64encode(uuid.uuid4().bytes)
+        salt = uuid.uuid4().hex
         print("Password: " + password)
-        print("Salt: " + str(salt))
+        print(salt)
 
-        pass_salt =''
-        pass_salt += password
-        pass_salt += str(salt)
-        final_hash = hashlib.sha256(pass_salt.encode()).hexdigest()
+        print("Password with Salt:" + password + salt)
+        pws = "" + password + salt 
+        #pwse = pws.encode
+        final_hash = hashlib.sha256(pws.encode()).hexdigest()
         print("Hash and Salt: " + final_hash)
 
-        sql = """INSERT INTO Hash (UserName, Salt, Hash) VALUES (%s,%s,%s)"""
+        sql_in = """INSERT INTO Hash (UserName, Salt, Hash) VALUES (%s,%s,%s)"""
         insert_tuple = (user_name, salt, final_hash)
-        cursor.execute(sql, insert_tuple)
+        cursor.execute(sql_in, insert_tuple)
        
         #cursor.execute(sql)
 
@@ -43,6 +41,44 @@ try:
         # If you INSERT, UPDATE or CREATE, the connection is not autocommit by default.
         # So you must commit to save your changes. 
         connection.commit()
+
+        #get password from database
+
+        user_name = input("Enter a username\n" )
+        print("Username: " + user_name)
+
+        password2 = input("Enter a password:\n" )
+
+        sql_out = "SELECT * FROM Hash WHERE UserName = %s"
+              
+        cursor.execute(sql_out, user_name)
+
+        for result in cursor:
+             username_r = result['UserName']
+             salt_r = result['Salt']
+             hash_r = result['Hash']
+             print("Password Entered: " + password2)
+             print("Salt from DB: " + salt_r)
+             
+
+             pass2_salt_r = "" + password2 + salt_r
+             print("Password+salt: " + pass2_salt_r)
+             check_hash = hashlib.sha256(pass2_salt_r.encode()).hexdigest()
+
+             print("Check Hash: " + check_hash )
+             if (check_hash == hash_r):
+                 print("\nAccess Granted: Password Accepted\n")
+        print ("Username DB: " + str(username_r))
+        print("Salt DB: " + str(salt_r))
+        print("Hash_DB: " + str(hash_r))
+
+
+
+
+
+
+
+
         
 
 finally:
